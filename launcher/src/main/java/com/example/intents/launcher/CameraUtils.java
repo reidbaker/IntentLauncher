1710d903c7e90b2ae4e2e70124554d9fe1cbe79b
Reid Baker <rbaker@evernote.com>
2014-02-07 01:50:14 -0600

    get images works, send images works with most apps

---
+++ launcher/src/main/java/com/example/intents/launcher/CameraUtils.java
@@ -0,0 +1,59 @@
+package com.example.intents.launcher;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+
+public class CameraUtils {
+
+    public static void addPhotoToMediaStore(Context context, Uri uri) {
+        MediaScannerConnection.scanFile(context, new String[]{uri.getPath()}, new String[]{"image/*"},
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.i("MediaStore", "Scanned " + path + ":");
+                        Log.i("MediaStore", "-> uri=" + uri);
+                    }
+                });
+    }
+
+    public static boolean hasCamera(Context context) {
+        if (context == null || context.getPackageManager() == null) {
+            throw new IllegalArgumentException("Context and package manager must not be null");
+        }
+        PackageManager pm = context.getPackageManager();
+        if (pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            return true;
+        } else if (android.os.Build.VERSION.SDK_INT >= 9) {
+            return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
+        } else {
+            return false;
+        }
+    }
+
+
+    public static File getPhotoFile(Context context){
+        File path;
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            path = new File(Environment.getExternalStorageDirectory().getPath() + MainActivity.DEFAULT_IMAGE_DIR);
+        } else {
+            path = new File(context.getFilesDir().getPath());
+        }
+        path.mkdirs();
+        return new File(path, MainActivity.DEFAULT_IMAGE_NAME);
+    }
+
+    public static boolean removeTemporaryPhotoFile(File file) {
+        boolean success = false;
+        try {
+            success = file.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return success;
+    }
+}
--- launcher/src/main/java/com/example/intents/launcher/MainActivity.java
+++ launcher/src/main/java/com/example/intents/launcher/MainActivity.java
@@ -2,51 +2,141 @@ package com.example.intents.launcher;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.channels.FileChannel;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
     private View mGetImage;
     private View mEditImage;
 
-    private final static int REQUEST_CODE_PICK_IMAGE = 1;
+    private final static int REQUEST_CODE_TAKE_PHOTO = 1;
+    private final static int REQUEST_CODE_EDIT_PHOTO = 2;
 
-    private final static String DEFAULT_IMAGE_DIR = "Launcher_Images";
-    private final static String DEFAULT_IMAGE_NAME = "image.tmp";
+    public final static String DEFAULT_IMAGE_DIR = "/Launcher_Images/";
+    public final static String DEFAULT_IMAGE_NAME = "image.tmp";
 
-    Uri mUri;
+    private static final String PHOTO_KEY = "photo_key";
+
+    private File mPhotoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //Tell's the activity how to layout the visuals
+        setContentView(R.layout.main_activity); // File located in res/layout
+
         mGetImage = findViewById(R.id.get_image);
         mGetImage.setOnClickListener(this);
+
         mEditImage = findViewById(R.id.edit_image);
         mEditImage.setOnClickListener(this);
 
+        //If this is not the first time we launched then get the photo file
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(PHOTO_KEY)) {
+                mPhotoFile = (File) savedInstanceState.getSerializable(PHOTO_KEY);
+                setImageView(mPhotoFile);
+            }
+        }
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == mGetImage.getId()){
+            getImage();
+        } else if (v.getId() == mEditImage.getId()) {
+            editImage();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        //If we have the photo file then save it on rotation
+        if (mPhotoFile != null) {
+            outState.putSerializable(PHOTO_KEY, mPhotoFile);
+        }
+    }
+
+    private void getImage() {
+        if (!CameraUtils.hasCamera(this)) {
+            Toast.makeText(this, R.string.feature_unavailable, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            mPhotoFile = CameraUtils.getPhotoFile(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, R.string.no_pic_captured, Toast.LENGTH_LONG).show();
+            CameraUtils.removeTemporaryPhotoFile(mPhotoFile);
+            return;
+        }
+
+        Intent cameraIntent = new Intent();
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mPhotoFile));
+        cameraIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        try {
+            startActivityForResult(cameraIntent, REQUEST_CODE_TAKE_PHOTO);
+        } catch (Exception ex) {
+            Toast.makeText(this, R.string.no_capture_activity_found, Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private void editImage() {
+        Toast.makeText(this, R.string.edit_image, Toast.LENGTH_SHORT).show();
+
+        Intent editImage = new Intent(Intent.ACTION_SEND);
+        editImage.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(mPhotoFile));
+        editImage.setType("image/jpeg");
+        Intent selector = Intent.createChooser(editImage, getString(R.id.edit_image));
+        startActivity(selector);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (resultCode == Activity.RESULT_CANCELED && requestCode == REQUEST_CODE_TAKE_PHOTO)
+            CameraUtils.removeTemporaryPhotoFile(mPhotoFile);
+        else if(resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_TAKE_PHOTO) {
+            //Converts java.net.uri to android.net uri
+            CameraUtils.addPhotoToMediaStore(this, Uri.fromFile(mPhotoFile));
+
+            setImageView(mPhotoFile);
+        }
+
+        if(resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_EDIT_PHOTO) {
+            Toast.makeText(this, getString(R.string.edit_image) + "success", Toast.LENGTH_SHORT).show();
+        }
+
+        super.onActivityResult(requestCode, resultCode, intent);
+    }
+
+    private void setImageView(File image) {
+        Bitmap myBitmap = BitmapFactory.decodeFile(image.getAbsolutePath());
+
+        ImageView myImage = (ImageView) findViewById(R.id.image_view);
+        myImage.setImageBitmap(myBitmap);
+    }
+
+    //Boiler plate code below here
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -62,79 +152,5 @@ public class MainActivity extends Activity implements View.OnClickListener{
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-        if(v.getId() == mGetImage.getId()){
-            getImage();
-        } else if (v.getId() == mEditImage.getId()) {
-            editImage();
-        }
-    }
-
-    private void getImage() {
-        Toast.makeText(this, "get image", Toast.LENGTH_SHORT).show();
-        Intent getImageContent = new Intent(Intent.ACTION_GET_CONTENT);
-        getImageContent.setType("image/*");
-        Intent selector = Intent.createChooser(getImageContent, getString(R.id.get_image));
-        startActivityForResult(selector, REQUEST_CODE_PICK_IMAGE);
-    }
-
-    private void editImage() {
-        Toast.makeText(this, "edit image", Toast.LENGTH_SHORT).show();
-        Intent editImage = new Intent(Intent.ACTION_EDIT);
-        File imageOutputDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), DEFAULT_IMAGE_DIR);
-        File outFile = new File(imageOutputDir, DEFAULT_IMAGE_NAME);
-        editImage.setDataAndType(Uri.fromFile(outFile), "image/*");
-        Intent selector = Intent.createChooser(editImage, getString(R.id.edit_image));
-        startActivity(selector);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        mUri = null;
-        if (intent != null) {
-            mUri = intent.getData();
-            String path = getPath(mUri);
-            File imageInput = new File(path);
-            File imageOutputDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), DEFAULT_IMAGE_DIR);
-            if (!imageOutputDir.exists()) {
-                imageOutputDir.mkdir();
-            }
-            File imageOutput = new File(imageOutputDir, DEFAULT_IMAGE_NAME);
-            try {
-                copyFile(imageInput, imageOutput);
-            } catch (IOException e) {
-                Log.e(MainActivity.class.getSimpleName(), "Error getting image from " + mUri.toString());
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, intent);
-    }
-
-    private void copyFile(File sourceFile, File destFile) throws IOException {
-        if (!sourceFile.exists()) {
-            return;
-        }
-
-        FileChannel source = null;
-        FileChannel destination = null;
-        source = new FileInputStream(sourceFile).getChannel();
-        destination = new FileOutputStream(destFile).getChannel();
-        if (destination != null && source != null) {
-            destination.transferFrom(source, 0, source.size());
-        }
-        if (source != null) {
-            source.close();
-        }
-        if (destination != null) {
-            destination.close();
-        }
-    }
-    private String getPath(Uri uri) {
-        String[] projection = {MediaStore.Images.Media.DATA };
-        Cursor cursor = this.getContentResolver().query(uri, projection, null, null, null);
-        cursor.moveToFirst();
-        return cursor.getString(0);
     }
 }
